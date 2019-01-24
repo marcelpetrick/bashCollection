3b2a3efdb15a6a05d0f69efb2e8d58024b8cf42d
Marcel Petrick <petrick@instrumentsystems.com>
2019-01-24 10:22:41 +0100

    cleaned! Invoke with: $ git diff --name-only HEAD~1 HEAD | xargs -L1 ../removeTrailing.sh
    Change-Id: I85052cc0bed02940a39fc79407c603cfc6291f4e

--- removeTrailing.sh
+++ removeTrailing.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 # remove trailing whitespace and convert to spaces
 
+# todo: maybe check for existance of the input file ..
+
 echo remove trailing whitespace
 sed -i 's/[ \t]*$//' "$1"
 
 echo replace tabs with four spaces
-#sed -i 's/\t/    /g' {} \;
 sed -i 's/\t/    /g' "$1"
-#echo "$1new"
-#expand "$1" | tee -a "$1new"
-#mv "$1new" "$1"
