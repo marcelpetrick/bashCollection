1ac37f006652c187b0b4f9d105530b9294754449
Marcel Petrick <mail@marcelpetrick.it>
2019-10-15 14:43:15 +0200

    randomReviewer: added new colleague

--- randomReviewer.sh
+++ randomReviewer.sh
@@ -9,7 +9,7 @@
 # license:	GNU General Public License v3.0
 
 # initialize the array
-array=(HGA GSC NKU MPE MDR KWL DAD RSH JLU)
+array=(HGA GSC NKU MPE MDR KWL DAD RSH JLU GSH)
 
 echo "---------------------------------------------------"
 
