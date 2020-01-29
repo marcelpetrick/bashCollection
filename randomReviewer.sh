00e10a24bd854a65874d85beaee0a9af0e12cfb8
Marcel Petrick <mail@marcelpetrick.it>
2020-01-29 22:17:42 +0100

    Update randomReviewer.sh

--- randomReviewer.sh
+++ randomReviewer.sh
@@ -10,7 +10,7 @@
 # license:	GNU General Public License v3.0
 
 # initialize the array with all potential reviewers
-array=(DAD GSH HGA KWL MDR MPE NKU JLU RMA RSH)
+array=(DAD GSH HGA KWL MDR MPE NKU RMA RSH)
 
 echo "---------------------------------------------------"
 
