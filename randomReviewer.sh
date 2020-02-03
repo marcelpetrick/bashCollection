1511cdf867bfda5ce52aa6334d1be38e4ed3ff0f
Marcel Petrick <mail@marcelpetrick.it>
2020-02-03 13:05:26 +0100

    randomReviewer: added new SW-developer

--- randomReviewer.sh
+++ randomReviewer.sh
@@ -10,7 +10,7 @@
 # license:	GNU General Public License v3.0
 
 # initialize the array with all potential reviewers
-array=(DAD GSH HGA KWL MDR MPE NKU RMA RSH)
+array=(DAD GSH HGA KWL MDR MPE NKU RMA RSH BUN)
 
 echo "---------------------------------------------------"
 
