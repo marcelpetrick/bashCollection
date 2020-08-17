41fb0f2e2c8276090e5dd350a11e8ba099263e92
Marcel Petrick <mail@marcelpetrick.it>
2020-08-17 17:42:16 +0200

    randomReviewer: updated list

--- randomReviewer.sh
+++ randomReviewer.sh
@@ -10,7 +10,7 @@
 # license:	GNU General Public License v3.0
 
 # initialize the array with all potential reviewers
-array=(DAD GSH HGA KWL MDR MPE NKU AVI RSH_ext BUN_ext VGA_ext)
+array=(AVI DAD DNU GSH HGA KWL MDR MPE NKU RSH_ext BUN_ext VGA_ext)
 
 echo "---------------------------------------------------"
 
